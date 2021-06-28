96916d40b507d3b0010f2bd87cfec448881ef661
shreelimbkar <limbkarshree@gmail.com>
2021-06-28 23:53:17 +0100

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [initialData, setInitialData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((data) => setInitialData(data));
+  }, []);
+  return (
+    <div className="App">
+      <h1>{initialData.title}</h1>
+    </div>
+  );
+}
+
+export default App;
