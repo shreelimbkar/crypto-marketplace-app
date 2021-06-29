ec9af85a75b057f1fba839da90427846a2151f50
shreelimbkar <limbkarshree@gmail.com>
2021-06-29 11:11:53 +0100

    moved back app.py on root

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask.helpers import send_from_directory
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__, static_folder='frontend/build', static_url_path='')
+CORS(app)
+
+
+@app.route('/api', methods=['GET'])
+@cross_origin()
+def api():
+    return {
+        'userid': 1,
+        'title': 'Flask react Application -SHRI-NEW',
+        'completed': False
+    }
+
+
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+
+if __name__ == "__main__":
+    app.run()
