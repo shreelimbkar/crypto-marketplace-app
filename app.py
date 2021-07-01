065be8267e33c49bd7d484eb685cadc5ffd3fe17
shreelimbkar <limbkarshree@gmail.com>
2021-07-01 11:11:06 +0100

    added users api and fetch users from Supadatabase

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ from flask import Flask
 from flask.helpers import send_from_directory
 from flask_cors import CORS, cross_origin
 
+import os
+import requests
+
 app = Flask(__name__, static_folder='frontend/build', static_url_path='')
 CORS(app)
+
+
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
 
 
 @app.route('/api', methods=['GET'])
@@ -16,9 +24,19 @@ def api():
     }
 
 
-@app.route('/')
-def serve():
-    return send_from_directory(app.static_folder, 'index.html')
+@app.route('/api/users', methods=['GET'])
+@cross_origin()
+def users():
+    print("SUPABASE_URL: ", os.getenv('SUPABASE_URL'))
+    print("SUPABASE_API_KEY: ", os.getenv('SUPABASE_API_KEY'))
+    SUPABASE_URL = os.getenv('SUPABASE_URL')
+    SUPABASE_HEADERS = {
+        'apikey': os.getenv('SUPABASE_API_KEY'),
+        'Authorization': 'Bearer' + os.getenv('SUPABASE_API_KEY')
+    }
+    response = requests.get(f'{SUPABASE_URL}/users', headers=SUPABASE_HEADERS)
+
+    return response.text
 
 
 if __name__ == "__main__":
