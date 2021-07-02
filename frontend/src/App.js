eb4f94f42b327398f921b4529052822bd33d86c0
shreelimbkar <limbkarshree@gmail.com>
2021-07-03 00:20:38 +0100

    updated app.js and added build static folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-import { Login } from './components/user';
+import { Login } from "./components/user";
 
 function App() {
   return (
-    // <Router>
-    //   <div>
-    //     <Switch>
-    //       <Route path="/" component={Login} exact />
-    //     </Switch>
-    //     </div>
-    // </Router>
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" component={Login} exact />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
